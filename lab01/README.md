6bd51f571d7b7176d854eee14066697c4556fd5e
Jeffrey Moniz <146683404+jeffonsicle@users.noreply.github.com>
2024-09-10 22:06:03 +0000

    lab01 is doe

---
+++ lab01/README.md
@@ -0,0 +1,10 @@
+#DESCRIPTION
+Solutions for lab01
+#AUTHOR
+Name: Jeffrey Moniz
+Email: monizj4@mymacewan.ca
+#INSTALLATION
+#BUGS
+#CONTRIBUTE
+#CREDITS
+#LICENSE
---
+++ lab01/src/file.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+//creates the main function with void parameters
+int main() 
+{
+    //prints the string of text
+    printf("Welcome to CMPT 201");
+    //exits the function
+    return 0;
+}
