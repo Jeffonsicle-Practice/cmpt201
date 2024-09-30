8c642b7658c42a50dfeff5b8adfdb7b129af8071
Jeffrey Moniz <146683404+jeffonsicle@users.noreply.github.com>
2024-09-30 00:56:31 +0000

    hello

--- lab03/src/gravity.c
+++ lab03/src/gravity.c
@@ -1,15 +1,26 @@
 #include<stdio.h>
 #include"planets.h"
 #include<string.h>
-int main(char planet, int distance)
+#include<stdlib.h>
+int main(int argc, char *argv[])
 {
-    if(strcmp(planet,"mercury"))
+    int distance = atoi(argv[2]);
+
+    if(strcmp(argv[1],"mercury") == 0)
     {
-       printf("%f, %f, %d\n",mercuryMass, mercuryRad, distance);
+        printf("%s, %s",argv[1],argv[2]);
+        printf("%f, %f,%d\n",mercuryMass, mercuryRad,distance);
         return 0; 
     }
+    else if(strcmp(argv[0],"venus") == 0){}
+    else if(strcmp(argv[0],"earth") == 0){}
+    else if(strcmp(argv[0],"mars") == 0){}
+    else if(strcmp(argv[0],"jupiter") == 0){}
+    else if(strcmp(argv[0],"saturn") == 0){}
+    else if(strcmp(argv[0],"uranus") == 0){}
+    else if(strcmp(argv[0],"neptune") == 0){}
     else{
-        printf("you piece of ####");
+        printf("you piece of ####\n");
         return 0;
     }
 }
--- lab03/src/planets.h
+++ lab03/src/planets.h
@@ -1,39 +1,34 @@
 #include<math.h>
 #include<stdio.h>
 
-struct{
-    float mass;
-    float radius;
-} mercury = {500,300};
+#define mercuryMass 500.0
 
-#define mercuryMass 500
+#define mercuryRad 300.0
 
-#define mercuryRad 300
+#define venusMass 8685e24
 
-#define venus.mass 8685e24
+#define earthMass 5.9736e24
 
-#define earth.mass 5.9736e24
+#define marsMass  0.64185e24
 
-#define mars.mass  0.64185e24
+#define jupiterMass 1898.6e24
 
-#define jupiter.mass 1898.6e24
+#define saturnMass 568.46e24
 
-#define saturn.mass 568.46e24
+#define uranusMass  86.832e24
 
-#define uranus.mass  86.832e24
+#define neptuneMass 102.43e24
 
-#define neptune.mass 102.43e24
+#define venusRad 6051.8e3
 
-#define venus.radius 6051.8e3
+#define earthRad 6378.1e3
 
-#define earth.radius 6378.1e3
+#define marsRad  3396.2e3
 
-#define mars.radius  3396.2e3
+#define jupiterRad 71492.0e3
 
-#define jupiter.radius 71492.0e3
+#define saturnRad 60268.0e3
 
-#define saturn.radius 60268.0e3
+#define uranusRad 25559.0e3
 
-#define uranus.radius 25559.0e3
-
-#define neptune.radius 24764.0e3
+#define neptuneRad 24764.0e3
