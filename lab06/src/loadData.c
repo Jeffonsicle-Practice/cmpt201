620f6befb3fd3dda5a33c1c3e5f4c60fc89f59ab
Jeffrey Moniz <146683404+jeffonsicle@users.noreply.github.com>
2024-10-19 18:25:43 +0000

    lab06

--- lab06/src/loadData.c
+++ lab06/src/loadData.c
@@ -1,11 +1,21 @@
 
 
-int loadData(char* filename, int* stationNumber, double* temp, double* pres)
+int c
 {
     //bob
 }
 
 int main(void)
 {
-    //bob again
+    FILE *fp1, *fp2. *fp3;
+
+    fp1 = fopen("file1.txt", "r");
+    fp2 = fopen("file2.txt", "r");
+    fp3 = fopen("file3.txt", "r");
+
+    if(fp1 == NULL || fp2 == NULL || fp3 == NULL)
+    {
+        printf("file does not exist\n");
+        return 1;
+    }
 }
