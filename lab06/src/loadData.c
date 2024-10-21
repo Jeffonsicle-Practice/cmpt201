4b8e756afbb61ffc75d0e5eaa5f877419adc675c
Jeffrey Moniz <146683404+jeffonsicle@users.noreply.github.com>
2024-10-21 17:15:55 +0000

    lab06

--- lab06/src/loadData.c
+++ lab06/src/loadData.c
@@ -3,31 +3,49 @@
 
 int loadData(char* filename, int* stationNumber, double* temp, double* pres)
 {
-    FILE *fp;
+    /*
+    this function takes the filename and opens the file, it then checks to see if it exists. if it does exist
+    the function checks the format of the file and then takes the stationNumber, temp, and pres from the file
+    and loads the value in the pointers to be used in the main function
+    */
 
+    //opens the file
+    FILE *fp;
     fp = fopen(filename, "r");
 
+    //checks to see if the file exists
     if(fp == NULL)
     {
         printf("file does not exist\n");
         return 1;
     }
 
+    //checks the format of the file, if the format is good save the values in the pointer and close the file
     if(fscanf(fp, "station: %d\n",stationNumber) != 1 || fscanf(fp, "pressure [kPa] : %lf\n",pres) != 1 || fscanf(fp, "Temperature [C]: %lf\n",temp) != 1)
     {
         fclose(fp);
         return 1;
     }
 
+    //close the file and exit the function if the error checking is good
     fclose(fp);
     return 0;
 }
 
 int main(void)
 {
+    /*
+    this main function creates the 3 variables: staionNum, temp and pres. the function then takes these variables
+    as pointers and the file name and calls the loadData function to do error checking and set the values in the 
+    pointers. The function has three test cases for three different files
+    */
+
+    //initializes variables stationNum, temp and pres
     int stationNum;
     double temp, pres;
 
+
+    //calls the function loadData for file1, it then checks to see if the function execution was successful or not
     if(loadData("file1.txt",&stationNum,&temp,&pres) == 0)
     {
         printf("data loaded successfully: %d, %lf, %lf\n",stationNum,temp,pres);
@@ -37,8 +55,11 @@ int main(void)
         printf("error loading data from file1.txt\n");
     }
 
+    //creates a space for the printing
     printf("\n");
 
+
+    //calls the function loadData for file2, it then checks to see if the function execution was successful or not
     if(loadData("file2.txt",&stationNum,&temp,&pres) == 0)
     {
         printf("data loaded successfully: %d, %lf, %lf\n",stationNum,temp,pres);
@@ -48,8 +69,11 @@ int main(void)
         printf("error loading data from file2.txt\n");
     }
 
+    //creates a space for the printing
     printf("\n");
 
+
+    //calls the function loadData for file3, it then checks to see if the function execution was successful or not
     if(loadData("file3.txt",&stationNum,&temp,&pres) == 0)
     {
         printf("data loaded successfully: %d, %lf, %lf\n",stationNum,temp,pres);
@@ -59,5 +83,7 @@ int main(void)
         printf("error loading data from file3.txt\n");
     }
 
+
+    //exits the program
     return 0;
 }
